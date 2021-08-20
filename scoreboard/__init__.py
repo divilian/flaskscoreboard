62e85efdf980aad2724dfd3982269c50df6745d0
Stephen <stephen@umw.edu>
2021-08-20 13:23:28 -0400

    Replace factory with global app variable (just because).

--- scoreboard/__init__.py
+++ scoreboard/__init__.py
@@ -3,22 +3,20 @@ import os
 
 from flask import Flask
 
-def create_app():
-    xpapp = Flask(__name__, instance_relative_config=True)
-    xpapp.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(xpapp.instance_path,"xp.sqlite")
-    )
+xpapp = Flask(__name__, instance_relative_config=True)
+xpapp.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(xpapp.instance_path,"xp.sqlite")
+)
 
-    try:
-        os.makedirs(xpapp.instance_path, exist_ok=True)
-    except OSError as e:
-        print(f"Can't create inst path! {xpapp.instance_path}")
+try:
+    os.makedirs(xpapp.instance_path, exist_ok=True)
+except OSError as e:
+    print(f"Can't create inst path! {xpapp.instance_path}")
 
-    from . import db
-    db.init_app(xpapp)
-    
-    with xpapp.app_context():
-        from . import routes
+from . import db
+db.init_app(xpapp)
 
-    return xpapp
+with xpapp.app_context():
+    from . import routes
+
