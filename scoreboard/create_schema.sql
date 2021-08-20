0649fb6ecaac0f2d4970e4d5ba9566643df06934
Stephen <stephen@umw.edu>
2021-08-20 14:02:03 -0400

    Disallow nulls; add auto-timestamp.

--- scoreboard/create_schema.sql
+++ scoreboard/create_schema.sql
@@ -1,16 +1,16 @@
 
 DROP TABLE IF EXISTS `chars`;
 CREATE TABLE `chars` (
-  `realname` varchar(40) DEFAULT NULL,
+  `realname` varchar(40) NOT NULL,
   `charname` varchar(40) PRIMARY KEY NOT NULL
 );
 
 DROP TABLE IF EXISTS `xp`;
 CREATE TABLE `xp` (
-  `username` varchar(40) DEFAULT NULL,
-  `xps` int(11) DEFAULT NULL,
+  `username` varchar(40) NOT NULL,
+  `xps` int(11) NOT NULL,
   `tag` varchar(100) DEFAULT NULL,
-  `thetime` datetime DEFAULT NULL
+  `thetime` datetime DEFAULT CURRENT_TIMESTAMP
 );
 
 DROP TABLE IF EXISTS `secret`;
