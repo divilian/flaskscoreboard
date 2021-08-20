5d3b074b02329af1b828a64dcee0e0180db2df1c
Stephen <stephen@umw.edu>
2021-08-19 21:17:28 -0400

    Hello world.

---
+++ scoreboard/__init__.py
@@ -0,0 +1,22 @@
+
+import os
+
+from flask import Flask
+
+def create_app():
+    xpapp = Flask(__name__, instance_relative_config=True)
+    xpapp.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(xpapp.instance_path,"xp.sqlite")
+    )
+
+    try:
+        os.makedirs(xpapp.instance_path, exist_ok=True)
+    except OSError as e:
+        print(f"Can't create inst path! {xpapp.instance_path}")
+
+    @xpapp.route('/hello')
+    def hello():
+        return "hello scoreboard!"
+
+    return xpapp
