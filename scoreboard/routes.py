7810538c6b916ed391b16df1f03e9856aff00b5c
Stephen <stephen@umw.edu>
2021-08-22 19:10:44 -0400

    Add faculty scoreboard option.

--- scoreboard/routes.py
+++ scoreboard/routes.py
@@ -1,5 +1,5 @@
 
-from flask import current_app, g, render_template
+from flask import current_app, g, render_template, request
 from scoreboard.db import get_db
 from scoreboard.levels import get_level
 import numpy as np
@@ -50,3 +50,54 @@ def student_scoreboard():
     
     return render_template("student_scoreboard.html",students=students,
         title="Student view")
+
+# URL is .../fac?hash=ajwerjaklerjkaejlrkj
+@current_app.route('/fac')
+def faculty_scoreboard():
+
+    db = get_db()
+
+    db_secret = db.execute("select secret from secret").fetchone()
+    if ('hash' in request.args and request.args['hash'] and
+        request.args['hash'] == db_secret['secret']):
+
+        db_results = db.execute(
+            """
+            select realname, username, sum(xps) as total,
+                    max(thetime) as latest_time from
+                xp join chars on username=charname
+            group by username
+            order by total desc
+            """).fetchall()
+        students = pd.DataFrame(db_results)
+        students.columns = ['realname','username','total','latest_time']
+
+        levels = np.array([], dtype='object')
+        images = np.array([], dtype='object')
+        grades = np.array([], dtype='object')
+        most_recents = np.array([], dtype='object')
+
+        for row in students.itertuples():
+
+            level, image, grade = get_level(row.total)
+            levels = np.append(levels, level)
+            images = np.append(images, image)
+            grades = np.append(grades, grade)
+
+            most_recent_tag = db.execute(
+                """
+                select tag from xp where username=?
+                order by thetime desc
+                limit 1
+                """, (row.username,)).fetchone()
+            most_recents = np.append(most_recents, most_recent_tag['tag'])
+
+        students['level'] = levels
+        students['image'] = images
+        students['grade'] = grades
+        students['most_recent'] = most_recents
+        
+        return render_template("faculty_scoreboard.html",students=students,
+            title="Faculty view")
+    else:
+        return "Nope."
